4ce19f00d90cdb851cdba7eedcd4fa8a2060cb7d
RajputanaRoyality <vaibhavrksingh6207@gmail.com>
2025-12-23 23:00:35 +0530

    finally

--- frontend/job_portal/src/layout/LoadingSpinner.jsx
+++ frontend/job_portal/src/layout/LoadingSpinner.jsx
@@ -8,7 +8,7 @@ const LoadingSpinner = () => {
         <div className="relative">
           <div className="animate-spin rounded-full h-16 w-16 border-4 border-blue-200 border-t-blue-600 mx-auto mb-4"></div>
           <div className="absolute inset-0 flex items-center justify-center">
-            <Briefcase className='w-6 h-6 text-blue-600'/>
+            <Briefcase className='w-6 h-6 text-blue-600' />
           </div>
         </div>
         <p className="text-gray-600 font-medium">
